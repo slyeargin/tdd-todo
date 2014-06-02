f1f79570fb2c96a416ec4f748597b34cb0acd6a7
Samantha Yeargin <slyeargin@gmail.com>
2014-06-02 16:21:13 -0500

    EOD

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//var Mongo = require('mongodb');
+var users = global.nss.db.collection('users');
+var bcrypt = require('bcrypt');
+var _ = require('lodash');
+
+class User{
+  static register(obj, fn){
+    User.findByEmail(obj.email, u=>{
+      if(u){
+        fn(null);
+      }else{
+        obj.password = bcrypt.hashSync(obj.password, 8);
+        obj = _.create(User.prototype, obj);
+        users.save(obj, o=>fn(o));
+      }
+    });
+  }
+
+  static findByEmail(email, fn){
+    users.findOne({email:email}, (e, u)=>fn(u));
+  }
+}
+
+module.exports = User;
---
+++ test/unit/user_spec.js
@@ -0,0 +1,51 @@
+/* global describe, it, before, beforeEach */
+/* jshint expr:true */
+
+'use strict';
+
+process.env.DBNAME = 'tddtodo-test';
+
+var expect = require('chai').expect;
+var Mongo = require('mongodb');
+var app = require('../../app/app');
+var request = require('supertest');
+var traceur = require('traceur');
+
+var User;
+
+describe('User', function(){
+  before(function(done){
+    request(app)
+    .get('/')
+    .end(function(){
+      User = traceur.require(__dirname + '/../../app/models/user.js');
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    global.nss.db.collection('users').drop(done);
+    var obj = {email:'sue@aol.com', password: '1234'};
+    User.register(obj, done());
+  });
+
+  describe('.register', function(){
+    it('should successfully register a user', function(){
+      var obj = {email:'bob@aol.com', password: '1234'};
+      User.register(obj, function(u){
+        expect(u).to.be.ok;
+        expect(u).to.be.an.instanceOf(User);
+        expect(u._id).to.be.an.instanceOf(Mongo.ObjectID);
+        expect(u.password).to.not.equal(obj.password);
+      });
+    });
+
+    it('should refuse to register a duplicate user', function(){
+      var obj = {email:'sue@aol.com', password: '1234'};
+      User.register(obj, function(u){
+        expect(u).to.be.null;
+      });
+    });
+
+  });
+});
